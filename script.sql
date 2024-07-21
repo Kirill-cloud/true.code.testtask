cfde68ac0041e4f3d409e00f60e238fff5236407
Konovalenko Kirill <71507068+Kirill-cloud@users.noreply.github.com>
2024-07-21 20:19:26 +0700

    Delete script.sql

--- script.sql
+++
@@ -1,9 +0,0 @@
-﻿START TRANSACTION;
-
-CREATE UNIQUE INDEX "IX_Priorities_Level" ON "Priorities" ("Level");
-
-INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
-VALUES ('20240721112742_UniqueIndex', '8.0.4');
-
-COMMIT;
-
